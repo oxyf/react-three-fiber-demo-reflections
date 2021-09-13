b68a860af39a96a0bed5001c459662e4eda750b5
oxyf <pnajimovich@gmail.com>
2021-09-13 20:42:07 -0300

    Updates

---
+++ precache-manifest.fc5a7d78363fdd12b06a65a1b5628e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db9dc8dda94cf3809d73547bce1efc2",
+    "url": "/react-three-fiber-demo-reflections/index.html"
+  },
+  {
+    "revision": "ef57957980f50f3061a0",
+    "url": "/react-three-fiber-demo-reflections/static/css/main.3ef7fbf4.chunk.css"
+  },
+  {
+    "revision": "ac242538ee0d6a9c5a96",
+    "url": "/react-three-fiber-demo-reflections/static/js/2.af972b60.chunk.js"
+  },
+  {
+    "revision": "ef57957980f50f3061a0",
+    "url": "/react-three-fiber-demo-reflections/static/js/main.cbb0e1a8.chunk.js"
+  },
+  {
+    "revision": "f24673009b6b6f42fc9a",
+    "url": "/react-three-fiber-demo-reflections/static/js/runtime~main.8e8894e1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-three-js-fiber-demo-reflections/precache-manifest.b96f28bc4bf2713044a67a61132a7f30.js"
+  "/react-three-fiber-demo-reflections/precache-manifest.fc5a7d78363fdd12b06a65a1b5628e37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-three-js-fiber-demo-reflections/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-three-fiber-demo-reflections/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
